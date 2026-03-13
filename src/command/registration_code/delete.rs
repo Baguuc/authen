d4874c78d490da55c974e418661145c418611390
Jan Żukowski <janzukowski@proton.me>
2026-03-13 20:04:38 +0100

    update: added delete_registration_code command

---
+++ src/command/registration_code/delete.rs
@@ -0,0 +1,30 @@
+use sqlx::{Acquire, Postgres};
+use tracing::instrument;
+use uuid::Uuid;
+
+#[derive(thiserror::Error, Debug)]
+pub enum RegistrationCodeDeletionError {
+    #[error("Code not exists.")]
+    NotExists,
+    #[error("Database error: {0}")]
+    Sqlx(#[from] sqlx::Error)
+}
+
+/// Command to delete a registration code from the database.
+#[instrument(name = "Creating a registration code", skip(db_conn))]
+pub async fn delete_registration_code<'a, A: Acquire<'a, Database = Postgres>>(db_conn: A, id: Uuid) -> Result<(), RegistrationCodeDeletionError> {
+    let mut db_conn = db_conn.acquire().await?;
+    
+    // the rest should be filled out by postgres automatically
+    let sql = "DELETE FROM registration_codes WHERE id = $1;";
+    let result = sqlx::query(sql)
+        .bind(id)
+        .execute(&mut *db_conn)
+        .await?;
+
+    if result.rows_affected() == 0 {
+        return Err(RegistrationCodeDeletionError::NotExists);
+    }
+
+    Ok(())
+}
--- src/command/registration_code/mod.rs
+++ src/command/registration_code/mod.rs
@@ -1,3 +1,5 @@
 pub mod create;
+pub mod delete;
 
 pub use create::create_registration_code;
+pub use delete::delete_registration_code;
