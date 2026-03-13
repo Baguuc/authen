9f587153c6215f164c660ee08206fa69e863871a
Jan Żukowski <janzukowski@proton.me>
2026-03-13 19:41:36 +0100

    update: added activate_user command

---
+++ src/command/user/activate.rs
@@ -0,0 +1,18 @@
+use sqlx::{Acquire, Postgres};
+use tracing::instrument;
+use uuid::Uuid;
+
+/// Command to update the user's 'active' column (activating the user).
+#[instrument(name = "Activating a user", skip(db_conn))]
+pub async fn activate_user<'a, A: Acquire<'a, Database = Postgres>>(db_conn: A, id: Uuid) -> Result<(), sqlx::Error> {
+    let mut db_conn = db_conn.acquire().await?;
+
+    // the rest should be filled out by postgres automatically
+    let sql = "UPDATE users SET active = true WHERE id = $1;";
+    let _ = sqlx::query(sql)
+        .bind(id)
+        .execute(&mut *db_conn)
+        .await?;
+
+    Ok(())
+}
--- src/command/user/mod.rs
+++ src/command/user/mod.rs
@@ -1,3 +1,5 @@
 pub mod create;
+pub mod activate;
 
 pub use create::create_user;
+pub use activate::activate_user;
