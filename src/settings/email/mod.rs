2134ebfcd3c1834d260cfc0012aca6869b62137b
Jan Żukowski <janzukowski@proton.me>
2026-03-21 11:24:07 +0100

    update: added settings options for the email customization feature

--- src/settings/email/mod.rs
+++ src/settings/email/mod.rs
@@ -3,7 +3,9 @@ use crate::model::email::Email;
 #[derive(serde::Deserialize, Clone)]
 pub struct EmailSettings {
     pub server: EmailServerSettings,
-    pub sender: Email
+    pub sender: Email,
+    pub registration: RegistrationEmailSettings,
+    pub login: LoginEmailSettings    
 }
 
 #[derive(serde::Deserialize, Clone)]
@@ -30,6 +32,20 @@ pub struct EmailSendEnpointHeaderSettings {
 pub struct EmailSendEnpointJsonFieldsSettings {
     pub from: String,
     pub to: String,
+    pub subject: String,
+    pub text_body: String,
+    pub html_body: String
+}
+
+#[derive(serde::Deserialize, Clone)]
+pub struct RegistrationEmailSettings {
+    pub subject: String,
+    pub text_body: String,
+    pub html_body: String
+}
+
+#[derive(serde::Deserialize, Clone)]
+pub struct LoginEmailSettings {
     pub subject: String,
     pub text_body: String,
     pub html_body: String
